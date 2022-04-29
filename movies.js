f1e4c355fb977727434823722b56ae4898a81452
aksmagic31 <sammyzhang31@gmail.com>
2022-04-28 20:21:50 -0700

    displaying data to main

--- movies.js
+++ movies.js
@@ -1,25 +1,54 @@
-//  // information about one movie
-  fetch('https://imdb-api.com/API/ExternalSites/k_vfmd1877/tt0411008')
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data.netflix);
-    var netflixLink = data.netflix;
-  }
 
-  );
+var myAPI = "k_vfmd1877"
+// movie title for the main page
+var movieTitle = ""
+
+// movie rating for the main page
+var movieRating = 0
+
+// this is the image url
+var imageUrl = ""
+
+var imageEl = document.querySelectorAll(".movieImg")
+var titleEl = document.querySelectorAll(".movieTitle")
+
+// function getMovieImage(){
+//   for (var i =0; i < 12; i++)
+//   {
+//     imageEl[i].src = imageUrl
+//   }
+// }
+// //  // information about one movie
+//   fetch('https://imdb-api.com/API/ExternalSites/k_vfmd1877/tt0411008')
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data.netflix);
+//     var netflixLink = data.netflix;
+//   }
+
+  // );
 
 //   // top 250
-  fetch('https://imdb-api.com/en/API/Top250Movies/k_vfmd1877')
+  fetch(`https://imdb-api.com/en/API/MostPopularMovies/${myAPI}`)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
     console.log(data);
-    for (var i = 0; i < data.length; i++) {
-      console.log(data.items[i].title);
-      var movieTitle = data.item[i].title;
+    for (var i = 0; i < 12; i++) {
+       movieTitle = data.items[i].fullTitle;
+       console.log(movieTitle)
+       titleEl[i] = movieTitle;
+       movieRating = data.items[i].imDbRating;
+       console.log(movieRating);
+         // this will generate the image for each of the box
+       imageUrl = data.items[i].image;
+       console.log(imageUrl)
+    
+       imageEl[i].src = imageUrl;
+       titleEl[i] = movieTitle;
     }
   });
 
@@ -38,37 +67,37 @@
 
 //   youtube trailer
 
-  fetch('https://imdb-api.com/en/API/YouTubeTrailer/k_vfmd1877/tt1375666')
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data.videoUrl);
-    var trailerUrl = data.videoUrl
-    console.log(trailerUrl)
-  });
+  // fetch('https://imdb-api.com/en/API/YouTubeTrailer/k_vfmd1877/tt1375666')
+  // .then(function (response) {
+  //   return response.json();
+  // })
+  // .then(function (data) {
+  //   console.log(data.videoUrl);
+  //   var trailerUrl = data.videoUrl
+  //   console.log(trailerUrl)
+  // });
 
 // random generator generating title from a keyword search
 
-  fetch('https://imdb-api.com/en/API/Keyword/k_vfmd1877/action-hero')
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    for (var i = 0; i < 10; i++) {
-      // .splice(index, howmany, item1, ....., itemX)
-      var randomTitleIndex =  randomNumberGenerator(data.items.length);
-      console.log(data.items[randomTitleIndex].title);
-      data.items.splice(randomTitleIndex,1)
-    }
+//   fetch('https://imdb-api.com/en/API/Keyword/k_vfmd1877/action-hero')
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//     for (var i = 0; i < 10; i++) {
+//       // .splice(index, howmany, item1, ....., itemX)
+//       var randomTitleIndex =  randomNumberGenerator(data.items.length);
+//       console.log(data.items[randomTitleIndex].title);
+//       data.items.splice(randomTitleIndex,1)
+//     }
   
-  });
+//   });
 
-function randomNumberGenerator (length){
-  // console.log("lenght ",length)
-   var randomNumber = Math.floor(Math.random()*length)
-  //  console.log("randomNumber " + randomNumber)
-   return randomNumber;
-}
-// randomNumberGenerator();
+// function randomNumberGenerator (length){
+//   // console.log("lenght ",length)
+//    var randomNumber = Math.floor(Math.random()*length)
+//   //  console.log("randomNumber " + randomNumber)
+//    return randomNumber;
+// }
+// // randomNumberGenerator();
