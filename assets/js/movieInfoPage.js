dc6a11556dbb03bddc5c75bb94f9941d85733898
Vy Nguyen <vynguyen205@Vys-MacBook-Air.attlocal.net>
2022-05-03 20:46:16 -0700

    styled movie info js and css

--- assets/js/movieInfoPage.js
+++ assets/js/movieInfoPage.js
@@ -6,32 +6,44 @@ function getMovie() {
         console.log(response);
         let movie = response.data;
   
-        let output = `
+        let output = /*html*/`
         <div class="row">
-            <div class="col-md-4">
-                <img src="${movie.Poster}" class="thumbnail">
-            </div>
-            <div class="col-md-8">
-                <h2>${movie.Title}</h2>
-                <ul class="list-group">
-                    <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
-                    <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
-                    <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
-                    <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
-                    <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
-                    <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
-                    <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
-                </ul>
-            </div>
+            <section class="mainContainer">
+                <div class= "posterWrapper">
+                    <img src="${movie.Poster}" class="thumbnail">
+                    <div class= "titleDetails">
+                        <h2>${movie.Title}</h2> 
+                        
+                        <div class="details">
+                            <span class="rated">${movie.Rated}</span>
+                            <div class="detail-item"> ${movie.Released} ▪️ ${movie.Genre}</div>
+                        </div>
+
+                        <div class="imbdRating">
+                            <span><strong>IMDB Rating:</strong> ${movie.imdbRating}</span>
+                            <button class="watchlistBtn" data-hover="Add to Watchlist"><i class="fa-solid fa-circle-plus"></i></button>
+                            <div class="trailerBtn"><i class="fa-solid fa-play"></i> Play Trailer</div>
+                        </div>
+                    
+                        <div class="movieOverview">   
+                            <h3>Overview</h3>
+                            <p>${movie.Plot}</p>
+                        </div>
+        
+                        <div class="movieCrew">
+                            <div class="crew-item"><strong>Director:</strong> ${movie.Director}</div>
+                            <div class="crew-item"><strong>Writer:</strong> ${movie.Writer}</div>
+                            <div class="crew-item"><strong>Actors:</strong> ${movie.Actors}</div>
+                        </div>
+                    </div>
+                </div>          
+            </section>
         </div>
+
         <div class="row">
-            <div class="well">
-                <h3>Plot</h3>
-                ${movie.Plot}
-                <hr>
-                <a href="https://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
-                <a href="./index.html" class="btn btn-default">Go Back To Search</a>
-            </div>
+            <hr>
+            <a href="https://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+            <a href="./index.html" class="btn btn-default">Go Back To Search</a>
         </div>
         `;
 
