7db9b55a46bd9cfb2210c656d30f45f410b24fce
jasmineranay <102012764+jasmineranay@users.noreply.github.com>
2022-05-03 19:17:07 -0700

    add watchlist retry

--- assets/js/indexPage.js
+++ assets/js/indexPage.js
@@ -1,24 +1,15 @@
-// API Keys
-var branfonApi = "k_1ucm7wp5";
-var vyAPI = "k_sr0i5ybd";
-var vy2API = "k_e2ggrgmv";
-var brianAPI ="k_tp8oqqm0"
-
-var myAPI = vyAPI;
 
 // Variables
-var movieTitle = ""
+var myAPI = "k_vfmd1877"
 var movieRating = 0
 var imageUrl = ""
 
 
 // HTML Targeting Variables
 var imageEl = document.querySelectorAll(".movieImg")
-var titleEl = document.querySelectorAll(".movieTitle")
+// var titleEl = document.querySelectorAll(".movieTitle")
 var ratingEl = document.querySelectorAll(".movieRating");
 var watchListBtn = document.querySelectorAll(".watchlistBtn");
-var search = document.getElementById('searchForm')
-var searchKey = document.getElementById('searchText');
 
 
 // Function for getting popular videos
@@ -30,55 +21,48 @@ function getPopularMovies() {
   })
   .then(function (data) {
     // Display API Data
-    // console.log(data);
+    console.log(data);
 
     // Loop thru the first 12 movies
     for (var i = 0; i < 12; i++) {
 
       // Movie Title
       movieTitle = data.items[i].title;
-        // console.log('movieTitle:', movieTitle)
+        console.log('movieTitle:', movieTitle)
 
       // IMDB Rating
       movieRating = data.items[i].imDbRating;
-        // console.log('movieRating:', movieRating);
-        
+        console.log('movieRating:', movieRating);
+      
       // Movie Picture
       imageUrl = data.items[i].image;
-        // console.log('imageURL:', imageUrl)
+        console.log('imageURL:', imageUrl)
 
     // Display the Data on Page
-    titleEl[i].textContent = movieTitle;
+    // titleEl[i].textContent = movieTitle;
     ratingEl[i].textContent = movieRating;
     imageEl[i].src = imageUrl;
+    watchListBtn[i].dataset.movie = data.items[i].title;
 
     }
   });
 }
+//
+for (var i = 0; i < watchListBtn.length; i++) {
+  watchListBtn[i].addEventListener("click", function(event){
+    console.log (event.target.attributes[1].value);
+    var Watchlist = JSON.parse(localStorage.getItem("list")) || [];
+      Watchlist.push(event.target.attributes[1].value)
+      localStorage.setItem('list', JSON.stringify(Watchlist))
 
+  })
 
-
-// Function for searching Movie
-function searchMovie() {
-  // Get Input Value
-  var searchInputVal = document.getElementById('searchText').value.trim();
-  var queryString = "./search.html?q=" + searchInputVal;
-
-  // Go to next page
-  location.assign(queryString)
 
 }
-
-// Listen for Enter Key to searchMovie
-searchKey.addEventListener('keypress', function (event) {
-  if (event.key === 'Enter' ) {
-    event.preventDefault()
-    searchMovie()
-  }
-})
-
 // Call Function on Page Load
 getPopularMovies();
+
+
 
 
 // UNUSED CODE
@@ -102,7 +86,7 @@ getPopularMovies();
 //     var netflixLink = data.netflix;
 //   }
 
-// );
+  // );
 
 
 // //   // most popular 
