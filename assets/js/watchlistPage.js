0536a346c289bea897c2d81c20fd02a9d08ef6cb
Vy Nguyen <vynguyen205@Vys-MacBook-Air.attlocal.net>
2022-05-04 22:47:29 -0700

    styled movie info

--- assets/js/watchlistPage.js
+++ assets/js/watchlistPage.js
@@ -1,3 +1,12 @@
+var imageEl = document.querySelectorAll(".movieImg")
+var titleEl = document.querySelectorAll(".movieTitle")
+var ratingEl = document.querySelectorAll(".movieRating");
+var watchListBtn = document.querySelectorAll(".watchlistBtn");
+
+var movieTitle = ""
+var movieRating = 0
+var imageUrl = ""
+
 var watchlistContainer = document.querySelector("#section-container")
 var Watchlist = JSON.parse(localStorage.getItem("list")) || [];
 for (var i = 0; i < Watchlist.length; i++) {
