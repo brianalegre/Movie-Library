f7348a3cc2935ddbf7e3e4160b9ba978932b6f2d
Brandon Sanchez <bsanchez26@live.com>
2022-05-05 19:37:35 -0700

    Got rid of unnecessary code

--- assets/js/movieInfoPage.js
+++ assets/js/movieInfoPage.js
@@ -1,55 +1,6 @@
 // HTML Targeting Variables
 var searchKey = document.getElementById('searchText');
-
-
-// Get Movie from URL
-var movieParam = document.location.search 
-var queryParam = movieParam.split('=').pop();
-    // Check Results in Dev Tools
-    console.log('movieParam', movieParam)
-    console.log('queryParam', queryParam)
-    // decodeURI opposite of encodeURI
-    console.log('decodueURI', decodeURI(queryParam))
-
-// Display Function on Load
-getMovies(decodeURI(queryParam))
-
-
-//to search and populate the movie
-function getMovies(searchText) {
-  axios.get('https://www.omdbapi.com?s=' + searchText + '&apikey=d7842ce1')
-    .then((response) => {
-      console.log(response);
-      let movies = response.data.Search;
-      let output = '';
-      $.each(movies, (_index, movie) => {
-        output += /*html*/`
-          <div class="col-md-3">
-            <div class="well">
-              <h5 class= "text-center">${movie.Title}</h5>
-            </div>
-            <div class="poster-cards">
-            <div class="img-container" style="background-image: url('${movie.Poster}')">
-              <div class="textOverlay textOverlay-blur" onclick="movieSelected('${movie.imdbID}')">
-                <span>MOVIE INFO</span>
-              </div>
-              </div>
-            </div>
-          </div>
-          `;
-          
-        });
-        $('#movies').html(output);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-      // //if already on search.html, don't do it
-      // if (!window.location.pathname.toLocaleLowerCase().includes("search")) {
-      //   window.location = `search.html?searchTerm=${searchText}`;
-      // } 
-    }
-    
+  
 // Function for searching Movie
 function searchMovie() {
   // Get Input Value
